0c5d24fef5c666e202f2dff406c8ab9707feb30d
Henrique Degrecci <hdegrecci@gmail.com>
2024-06-18 17:46:02 -0300

    feat: shadow-dom example

---
+++ frontend/shadow-dom/index.js
@@ -0,0 +1,5 @@
+const host = document.querySelector("#host");
+const shadow = host.attachShadow({ mode: "open" });
+const span = document.createElement("span");
+span.textContent = "I'm in the shadow DOM";
+shadow.appendChild(span);
